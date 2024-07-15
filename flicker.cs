62f6983092697e595a04f6c628d0ca0d2232cf61
Abin Jacob <106917779+abinjakob@users.noreply.github.com>
2024-07-15 11:13:12 +0200

    Improved logic for flicker timing

--- flicker.cs
+++ flicker.cs
@@ -1,41 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.RestService;
 using UnityEngine;
 
 public class flicker : MonoBehaviour
 {
     private Renderer rend;
-    // duration for 15Hz flicker (1/30 because we want 15 full cycles per second,
-    // so each state (on/off) lasts half the cycle)
-    public float flickerDuration = 1f / 30f;
+    // flicker frequency 
+    public float flickerFrequency = 15f;  
     // set color for on and off state 
     public Color onColor = Color.white;
     public Color offColor = Color.black;
     // total duration of the flicker 
     public float trialDuration = 60f;
+    // variable to track time
+    private float nextFlickerTime;
+    // variable to track flicker cycles
+    private float flickerDuration;
 
-    // Start is called before the first frame update
+
     void Start()
     {
+        // to ensure consistent frame rate in UNITY
+        Application.targetFrameRate = 60;
+        // get renderer component attached to game object 
         rend = GetComponent<Renderer>();
+        // as each state (on/off) lasts half the cycle
+        flickerDuration = 1f / (2 * flickerFrequency);
+        // initialise nextFlickerTime
+        nextFlickerTime = Time.time;
+        // start flicker coroutine
         StartCoroutine(FlickerBox());
     }
 
+
     IEnumerator FlickerBox()
     {
-        // variable to track time
+        // start time
+        float startTime = Time.time; 
+        // variable to track elapsed time
         float elapsedTime = 0f;
+        bool isOn = true;
+
         // loop until elapsed time reaches flicker time
         while (elapsedTime < trialDuration)
         {
-            // set to on state 
-            rend.material.color = onColor;
-            yield return new WaitForSeconds(flickerDuration);
-            // set to offstate 
-            rend.material.color = offColor;
-            yield return new WaitForSeconds(flickerDuration);
-            // increment elapsed time (each loop covers two flicker duration)
-            elapsedTime += 2 * flickerDuration;
+            if(Time.time >= nextFlickerTime)
+            {
+                // toggle color
+                rend.material.color = isOn ? onColor : offColor;
+                isOn = !isOn;
+                // schedule next flicker time
+                nextFlickerTime = Time.time + flickerDuration;
+            }
+            // update elapsed time
+            elapsedTime = Time.time - startTime;
+            // wait until next frame
+            yield return null;
         }
         // ensure box ends in off state
         rend.material.color = offColor;
