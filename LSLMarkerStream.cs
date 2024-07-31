f40594e5c9d4a828a68fc15e86ca225cf98a98ff
Abin Jacob <106917779+abinjakob@users.noreply.github.com>
2024-07-31 19:02:25 +0200

    Create LSLMarkerStream.cs

---
+++ LSLMarkerStream.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LSL;
+
+public class LSLMarkerStream : MonoBehaviour
+{
+    // declare LSL outlet and stream info
+    private StreamOutlet outlet;
+    private StreamInfo streamInfo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // create a new stream info
+        // info: stream name, stream type, channel count, irregular sampling rate, string format
+        streamInfo = new StreamInfo("EventMarkers", "Markers", 1, LSL.LSL.IRREGULAR_RATE, channel_format_t.cf_string);
+        
+        // create outlet
+        outlet = new StreamOutlet(streamInfo);
+    }
+
+    // method to write markers to LSL outlet
+    public void Write(string marker)
+    {
+        // create array to hold marker
+        string[] sample = new string[1];
+        sample[0] = marker; 
+
+        // push sample to outlet
+        outlet.push_sample(sample);
+    }
+}
